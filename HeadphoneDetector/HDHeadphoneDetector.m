3e145b8561e3ba3a2db4acff70d266a45af13fa9
Jun Wang <jun.wang@arubastudio.com>
2013-07-31 18:26:01 +0800

    headphone detect update

--- HeadphoneDetector/HDHeadphoneDetector.m
+++ HeadphoneDetector/HDHeadphoneDetector.m
@@ -49,18 +49,42 @@ void audioRouteChangeListenerCallback(void *inUserData, AudioSessionPropertyID i
 #pragma mark- Public Methods
 -(BOOL) currenstateArePlugged
 {
-	// thanks to: http://ios-dev-blog.com/how-to-check-that-headphones-are-attached-to-device/
+	// thanks to: http://blog.csdn.net/ysy441088327/article/details/8164120
 	BOOL result = NO;
     
 	CFStringRef route;
 	UInt32 propertySize = sizeof(CFStringRef);
 	if (AudioSessionGetProperty(kAudioSessionProperty_AudioRoute, &propertySize, &route) == 0)
     {
-		NSString *routeString = ( NSString *) route;
-		if ([routeString isEqualToString: @"Headphone"] == YES)
+        NSString *routeString = (NSString *)route;
+        DLog(@"routeString is %@", routeString)
+        
+        /* Known values of route:
+         * "Headset"
+         * "Headphone"
+         * "Speaker"
+         * "SpeakerAndMicrophone"
+         * "HeadphonesAndMicrophone"
+         * "HeadsetInOut"
+         * "ReceiverAndMicrophone"
+         * "Lineout"
+         */
+        
+        NSRange headphoneRange = [routeString rangeOfString : @"Headphone"];
+        NSRange headsetRange = [routeString rangeOfString : @"Headset"];
+        
+        if(headphoneRange.location != NSNotFound)
         {
-			result = YES;
-		}
+            result = YES;
+        }
+        else if (headsetRange.location != NSNotFound)
+        {
+            result = YES;
+        }
+        else
+        {
+            result = NO;
+        }
 	}
 	return result;
 }
@@ -74,6 +98,20 @@ void audioRouteChangeListenerCallback(void *inUserData, AudioSessionPropertyID i
 {
 	[[NSNotificationCenter defaultCenter] postNotificationName:HEADPHONE_NOT_PLUGGED object:self];
 }
+
+/*
+ -(void)headphoneIsPlugged
+ {
+    UInt32 audioRouteOverride = kAudioSessionOverrideAudioRoute_None;
+    AudioSessionSetProperty(kAudioSessionProperty_OverrideAudioRoute, sizeof(audioRouteOverride), &audioRouteOverride);
+ }
+ 
+ -(void)headphoneIsNotPlugged
+ {
+    UInt32 audioRouteOverride = kAudioSessionOverrideAudioRoute_Speaker;
+    AudioSessionSetProperty(kAudioSessionProperty_OverrideAudioRoute, sizeof(audioRouteOverride), &audioRouteOverride);
+ }
+ */
 
 #pragma mark- Listener Call Back
 void audioRouteChangeListenerCallback(void *inUserData, AudioSessionPropertyID inPropertyID, UInt32 inPropertySize, const void *inPropertyValue)
@@ -98,8 +136,27 @@ void audioRouteChangeListenerCallback(void *inUserData, AudioSessionPropertyID i
         {
             HDHeadphoneDetector *headphoneDetector = (HDHeadphoneDetector *)inUserData;
             NSString *routeString = (NSString *)route;
+            DLog(@"routeString is %@", routeString)
+            
+            /* Known values of route:
+             * "Headset"
+             * "Headphone"
+             * "Speaker"
+             * "SpeakerAndMicrophone"
+             * "HeadphonesAndMicrophone"
+             * "HeadsetInOut"
+             * "ReceiverAndMicrophone"
+             * "Lineout"
+             */
+            
+            NSRange headphoneRange = [routeString rangeOfString : @"Headphone"];
+            NSRange headsetRange = [routeString rangeOfString : @"Headset"];
 
-            if([routeString isEqualToString:@"Headphone"] == YES)
+            if(headphoneRange.location != NSNotFound)
+            {
+                [headphoneDetector headphoneArePlugged];
+            }
+            else if (headsetRange.location != NSNotFound)
             {
                 [headphoneDetector headphoneArePlugged];
             }
